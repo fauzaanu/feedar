b1733298eba84d057f3fff1df5503010dbbff51d
fauzaanu <fauzaangasim@gmail.com>
2024-02-22 01:04:35 +0500

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,6 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search_english, name='search_english'),
+    path('explore/<str:word>/', views.explore_word, name='explore_word'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
+from string import ascii_letters, digits, punctuation
+
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
 from mysite.settings.base import SITE_VERSION
+from dhivehi_nlp import dictionary
 
 
 # 30 days cache, example using site version to invalidate cache (increment to
@@ -10,3 +13,48 @@ from mysite.settings.base import SITE_VERSION
             key_prefix=SITE_VERSION)
 def home(request):
     return render(request, 'home/home.html')
+
+
+def search_english(request):
+    word = request.POST.get('word')
+
+    # remove punctuation and spaces
+    word = ''.join([char for char in word if char not in punctuation + digits])
+
+    # is it a dhivehi word or not?
+    if not word:
+        return render(request, 'home/search_english.html', {'meaning': 'Please enter a word'})
+    for letter in word:
+        if letter in ascii_letters:
+            return render(request, 'home/search_english.html', {'meaning': 'This is not a Dhivehi word'})
+
+    word = word.lower()
+    meaning = dictionary.get_definition(word)
+
+    context = {
+        'word': word,
+        'meaning': meaning,
+    }
+
+    return render(request, 'home/search_english.html', context)
+
+
+def explore_word(request, word):
+    # is it a dhivehi word or not? - todo:refactor later
+    if not word:
+        return render(request, 'home/search_english.html', {'meaning': 'Please enter a word'})
+    for letter in word:
+        if letter in ascii_letters:
+            return render(request, 'home/search_english.html', {'meaning': 'This is not a Dhivehi word'})
+
+    word = word.lower()
+    # remove punctuation and spaces
+    word = ''.join([char for char in word if char not in punctuation + digits])
+
+    meaning = dictionary.get_definition(word)
+
+    context = {
+        'word': word,
+        'meaning': meaning,
+    }
+    return render(request, 'home/search_english.html', context)
