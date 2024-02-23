ddad07ec021441d71fc31e73cb65610466bf97dd
fauzaanu <fauzaangasim@gmail.com>
2024-02-23 08:07:00 +0500

    uin

--- home/models.py
+++ home/models.py
@@ -38,7 +38,7 @@ class Webpage(models.Model):
 
 class SearchResponse(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
-    response = models.TextField()
+    response = models.TextField(db_index=False)
 
     def __str__(self):
         return f"{self.word} - {self.webpage} - {self.count}"
