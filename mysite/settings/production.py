e2c81b5959f15d05c9dd2f543105f4521fad816c
fauzaanu <fauzaangasim@gmail.com>
2024-02-23 07:04:50 +0500

    logging to console

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -44,3 +44,20 @@ CACHES = {
     }
 }
 
+# setup logging
+# https://docs.djangoproject.com/en/5.0/topics/logging/
+import os
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
