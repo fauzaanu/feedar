ceb8f2b63d5fd7beee405e63d4adbbc9db31df35
fauzaanu <fauzaangasim@gmail.com>
2024-02-25 00:10:25 +0500

    refactor 2.0 - remove helpers.py

--- home/helpers.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-from home.helpers.api_calls import google_custom_search_api
-from home.helpers.formatting import remove_all_english
-from home.models import SearchResponse, Word, Webpage
-
-
-
